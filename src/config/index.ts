12873c9367901cc5c4f2e1a18e6dafa494c4624e
haruki26 <h826.a519@gmail.com>
2026-02-16 12:18:24 +0900

    add: 自分の情報を作成
    無料枠だとエンドポイント数足りないので仕方なしハードコード

---
+++ src/config/index.ts
@@ -0,0 +1,16 @@
+import avatarImage from '/avatar.webp'
+import iconImage from '/icon.svg'
+
+export const MY_INFO = {
+  firstName: '陽生',
+  firstNameEn: 'haruki',
+  lastName: '久保',
+  lastNameEn: 'kubo',
+  birthday: {
+    year: 2005,
+    month: 8,
+    day: 26,
+  },
+  avatarImage: avatarImage,
+  iconImage: iconImage,
+}

182221c45167a5d929e9abeacd6615826a59bcc7
haruki26 <h826.a519@gmail.com>
2026-02-19 01:51:30 +0900

    add: トップビューを作成

---
+++ src/lib/toUpperFirst/index.ts
@@ -0,0 +1,2 @@
+export const toUpperFirst = (str: string) =>
+  `${str.charAt(0).toUpperCase()}${str.slice(1)}`
---
+++ src/routes/-components/WelcomeView/index.tsx
@@ -0,0 +1,23 @@
+import { MY_INFO } from '@/config'
+import { toUpperFirst } from '@/lib/toUpperFirst'
+
+const WelcomeView: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center gap-12 py-5 font-orbitron text-shadow-lg/40 text-shadow-secondary-100">
+      <div className="flex flex-col items-center gap-3">
+        <span className="text-3xl">Welcome</span>
+        <h1 className="flex flex-row gap-3 text-5xl">
+          <span>{toUpperFirst(MY_INFO.firstNameEn)}</span>
+          <span>{toUpperFirst(MY_INFO.lastNameEn)}</span>
+        </h1>
+      </div>
+      <p className="wrap-break-word flex flex-col gap-0.5 bg-base-300/5 text-center text-lg">
+        <span>このサイトでは</span>
+        <span>私のプロフィールや</span>
+        <span>作品についてまとめています</span>
+      </p>
+    </div>
+  )
+}
+
+export default WelcomeView

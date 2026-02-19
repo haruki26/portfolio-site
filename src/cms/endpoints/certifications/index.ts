c9d60086da87f85a9b9817bf52e5c8164dd270dd
haruki26 <h826.a519@gmail.com>
2026-02-19 22:33:01 +0900

    fix: typoの修正

--- src/cms/endpoints/certifications/index.ts
+++ src/cms/endpoints/certifications/index.ts
@@ -2,10 +2,10 @@ import { tryAsync } from '@/lib/result'
 import { fetchCertifications } from './functions'
 import { certificationMapper } from './mapper'
 
-const getCertificatons = async () =>
+const getCertifications = async () =>
   tryAsync(async () => {
     const res = await fetchCertifications()
     return res.contents.map(certificationMapper)
   })
 
-export { getCertificatons }
+export { getCertifications }

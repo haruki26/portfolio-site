c4b5d6c2f806e986c03aca548673df78778ed5be
haruki26 <h826.a519@gmail.com>
2026-02-28 02:16:51 +0900

    update: プリレンダリングできるように
    html-dom-parserがサーバー用のエントリを用いるように

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { createRequire } from 'node:module'
 import { fileURLToPath, URL } from 'node:url'
 import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
@@ -7,10 +8,30 @@ import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
+const require = createRequire(import.meta.url)
+
+// This needs to be resolved to the server-side version of html-dom-parser
+// since running this on the edge will throw an error.
+const htmlDomParserPath = require.resolve(
+  'html-dom-parser/lib/server/html-to-dom',
+)
+
 const config = defineConfig({
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    alias: [
+      {
+        find: 'html-dom-parser',
+        replacement: htmlDomParserPath,
+      },
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
+  },
+  ssr: {
+    optimizeDeps: {
+      include: ['html-react-parser'],
     },
   },
   plugins: [

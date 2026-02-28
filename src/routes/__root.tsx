42f50eceb67c3a150a5c791dd5668034226e568c
haruki26 <h826.a519@gmail.com>
2026-02-28 14:31:23 +0900

    update: 画像類の更新

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,17 +28,23 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Haruki Portfolio',
+        title: 'Haruki Portfolio site | 久保陽生のポートフォリオサイト',
       },
     ],
-    links: appCss
-      ? [
-          {
-            rel: 'stylesheet',
-            href: appCss,
-          },
-        ]
-      : [],
+    links: [
+      ...(appCss
+        ? [
+            {
+              rel: 'stylesheet',
+              href: appCss,
+            },
+          ]
+        : []),
+      {
+        rel: 'manifest',
+        href: '/manifest.json',
+      },
+    ],
   }),
   shellComponent: RootDocument,
 })

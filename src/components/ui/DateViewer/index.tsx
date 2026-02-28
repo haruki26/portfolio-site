951fcdf6a8060e2f36aa65f54e3811a57476dc73
haruki26 <h826.a519@gmail.com>
2026-03-01 00:35:43 +0900

    fix: divネストをやめてspanに

--- src/components/ui/DateViewer/index.tsx
+++ src/components/ui/DateViewer/index.tsx
@@ -6,13 +6,13 @@ interface Props {
 const DateViewer: React.FC<Props> = ({ date, separator = '-' }) => {
   return (
     <time dateTime={date.toISOString()}>
-      <div className="flex flex-row gap-0.5 text-lg">
+      <span className="flex flex-row gap-0.5 text-lg">
         <span>{date.getFullYear()}</span>
         <span>{separator}</span>
         <span>{date.getMonth() + 1}</span>
         <span>{separator}</span>
         <span>{date.getDate()}</span>
-      </div>
+      </span>
     </time>
   )
 }

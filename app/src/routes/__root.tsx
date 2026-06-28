d00aef88a86d61b5f731a1295bb6b61b86f8186a
haruki26 <h826.a519@gmail.com>
2026-06-28 22:28:47 +0900

    refactor: 未使用フォントを削除、プリコネクトを指定

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -41,6 +41,15 @@ export const Route = createRootRoute({
         rel: 'manifest',
         href: '/manifest.json',
       },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossOrigin: '',
+      },
     ],
     scripts: [
       {
